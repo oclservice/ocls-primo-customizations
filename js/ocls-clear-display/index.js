12c2b9f80d118b557bf1b932c61b81302deecb55
Thomas Guignard <tom@timtom.ch>
2023-05-02 09:22:49 -0400

    CLEAR API queries successful

--- js/ocls-clear-display/index.js
+++ js/ocls-clear-display/index.js
@@ -1,10 +1,38 @@
 // Display OUR/CLEAR permitted uses directly in Primo
 // Requires an URL to the OUR/CLEAR resource inside the
+import X2JS from 'x2js';
 
 angular
     .module('oclsClearDisplay', [])
+    .factory('oclsClearService', ['$http', '$sce',function($http, $sce){
+        function fetchOurData(baseUrl,resourceName){
+            let url = baseUrl + 'api/?tag=' + resourceName;
+            
+            var x2js = new X2JS();
+            
+            $sce.trustAsResourceUrl(url);
 
-    .controller('oclsClearDisplayController', ['$scope', function ($scope) {
+            return $http.get(url)
+                .then(
+                    function(response){
+                        return x2js.xml2js(response.data);
+                    },
+                    function(httpError){
+                        if (httpError.status === 404)return null;
+                        let error = "an error occured: oclsClearService callback: " + httpError.status;
+                        if (httpError.data && httpError.data.errorMessage) {
+                            error += ' - ' + httpError.data.errorMessage;
+                        }
+                        console.error(error);
+                        return null;
+                    }
+                );
+            }
+            return {
+                fetchOurData : fetchOurData
+            };
+        }])
+    .controller('oclsClearDisplayController', ['$scope', 'oclsClearService', function ($scope, oclsClearService) {
         var vm = this;
         this.$onInit = function() {
             $scope.$watch(
@@ -26,13 +54,25 @@ angular
                         for(var i = 0; i < services.length; i++){
                             console.log(services[i]);
                             var clearLink = services[i].publicNote.match(/href="(.+\.scholarsportal\.info\/licenses\/)([^"]+)\"/);
-                            if (clearLink.length > 0){
+                            console.log('Found CLEAR link');
+                            console.log(clearLink);
+                            if (clearLink){
                                 var clearBaseUrl = clearLink[1];
                                 console.log(clearBaseUrl);
                                 var clearResourceName = clearLink[2];
                                 console.log(clearResourceName);
-                                
-                                
+                                oclsClearService.fetchOurData(clearBaseUrl,clearResourceName)
+                                .then((data) => {
+                                    try{
+                                        if (!data)return;
+                                        // The data variable contains the license information as a JSON object.
+                                        console.log(data);
+                                    }
+                                    catch(e){
+                                        console.error("an error occured: oclsClearDisplayController:\n\n");
+                                        console.error(e.message);
+                                    }
+                                })
                                 
                             }
                             
